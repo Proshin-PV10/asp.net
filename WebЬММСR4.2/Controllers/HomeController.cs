3adf4b76b30e55330294b777238f06a7163c304a
Proshin-PV10 <167138897+Proshin-PV10@users.noreply.github.com>
2024-10-13 21:27:14 +0300

    Add files via upload

---
+++ WebЬММСR4.2/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using WebЬММСR1.Models;
+
+
+namespace WebЬММСR1.Controllers
+{
+	public class HomeController : Controller
+	{
+		private static PersonRepository db = new PersonRepository();
+		public IActionResult Index()
+		{
+			int hour = DateTime.Now.Hour;
+			ViewBag.Greeting = hour < 12 ? "Доброе утро" : "Добрый день";
+			ViewData["Mes"] = "хорошего настроения";
+			return View();
+		}
+	
+        [HttpGet]
+        public ViewResult InputData()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult InputData(Person p)
+        {
+            db.AddResponse(p);
+            return View("Hello", p);
+        }
+		public ViewResult OutputData()
+		{
+			ViewBag.Pers = db.GetAllResponses;
+			ViewBag.Count = db.NumberOfPerson;
+			return View("ListPerson");
+		}
+
+	}
+}
---
+++ WebЬММСR4.2/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WebЬММСR1.Models
+    {
+        public class Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public override string ToString() => $"{FirstName} {LastName}";
+       
+        }
+    }
